50bddbae6307165c87a2945ea099515a88d422c5
Tamires <tamirespatrocinioo@gmail.com>
2021-09-21 13:35:33 -0300

    Adicionar arquivos de projeto.

---
+++ BuscaIMG/TelaIMG.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace BuscaIMG
+{
+    partial class TelaIMG
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxImagem = new System.Windows.Forms.PictureBox();
+            this.btnBusca = new System.Windows.Forms.Button();
+            this.openFileDialogBusca = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pictureBoxImagem);
+            this.panel1.Location = new System.Drawing.Point(40, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(576, 420);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBoxImagem
+            // 
+            this.pictureBoxImagem.Location = new System.Drawing.Point(15, 15);
+            this.pictureBoxImagem.Name = "pictureBoxImagem";
+            this.pictureBoxImagem.Size = new System.Drawing.Size(546, 388);
+            this.pictureBoxImagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImagem.TabIndex = 0;
+            this.pictureBoxImagem.TabStop = false;
+            // 
+            // btnBusca
+            // 
+            this.btnBusca.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBusca.Location = new System.Drawing.Point(536, 495);
+            this.btnBusca.Name = "btnBusca";
+            this.btnBusca.Size = new System.Drawing.Size(80, 25);
+            this.btnBusca.TabIndex = 1;
+            this.btnBusca.Text = "&Buscar";
+            this.btnBusca.UseVisualStyleBackColor = true;
+            this.btnBusca.Click += new System.EventHandler(this.btnBusca_Click);
+            // 
+            // TelaIMG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(660, 558);
+            this.Controls.Add(this.btnBusca);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaIMG";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscar Imagem ::";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxImagem;
+        private System.Windows.Forms.Button btnBusca;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBusca;
+    }
+}
+
---
+++ BuscaIMG/TelaIMG.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaIMG
+{
+    public partial class TelaIMG : Form
+    {
+        public TelaIMG()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBusca_Click(object sender, EventArgs e)
+        {
+            openFileDialogBusca.Filter = "Todos os arquivos(*.*) | *.*";
+        
+             if(openFileDialogBusca.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxImagem.ImageLocation = openFileDialogBusca.FileName;
+            }
+        }
+    }
+}
